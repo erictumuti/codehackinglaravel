b17f5f147a700208f0663d16a186010bfe52cd23
erictumuti <erictumuti114@gmail.com>
2020-06-20 18:10:49 +0300

    admin master page set up

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,42 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Admin</title>
+    <title>Startmin - Bootstrap Admin Theme</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="css/app.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
-
-    <link href="css/blog-post.css" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.css" rel="stylesheet">
-    <link href="css/metisMenu.css" rel="stylesheet">
-    <link href="css/sb-admin-2.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
 
+    <!-- MetisMenu CSS -->
+    <link href="css/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/startmin.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="css/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
-
-
-
 </head>
-
-<body id="admin-page">
+<body>
 
 <div id="wrapper">
 
@@ -105,27 +103,25 @@
         {{--</ul>--}}
 
 
-
-
-
+        <!-- Sidebar -->
         <div class="navbar-default sidebar" role="navigation">
             <div class="sidebar-nav navbar-collapse">
+
                 <ul class="nav" id="side-menu">
                     <li class="sidebar-search">
                         <div class="input-group custom-search-form">
                             <input type="text" class="form-control" placeholder="Search...">
                                 <span class="input-group-btn">
-                                    <button class="btn btn-default" type="button">
+                                    <button class="btn btn-primary" type="button">
                                         <i class="fa fa-search"></i>
                                     </button>
                                 </span>
                         </div>
-                        <!-- /input-group -->
                     </li>
                     <li>
-                        <a href="/admin"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        <a href="#" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                     </li>
-
+                    <li>
                     <li>
                         <a href="#"><i class="fa fa-wrench fa-fw"></i>Users<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
@@ -155,7 +151,6 @@
                         </ul>
                         <!-- /.nav-second-level -->
                     </li>
-
 
                     <li>
                         <a href="#"><i class="fa fa-wrench fa-fw"></i>Categories<span class="fa arrow"></span></a>
@@ -237,11 +232,9 @@
                         <!-- /.nav-second-level -->
                     </li>
                     <li>
+
                         <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
-                            <li>
-                                <a href="#">Second Level Item</a>
-                            </li>
                             <li>
                                 <a href="#">Second Level Item</a>
                             </li>
@@ -251,42 +244,11 @@
                                     <li>
                                         <a href="#">Third Level Item</a>
                                     </li>
-                                    <li>
-                                        <a href="#">Third Level Item</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Third Level Item</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Third Level Item</a>
-                                    </li>
                                 </ul>
-                                <!-- /.nav-third-level -->
                             </li>
                         </ul>
-                        <!-- /.nav-second-level -->
                     </li>
-                    <li class="active">
-                        <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a class="active" href="blank.html">Blank Page</a>
-                            </li>
-                            <li>
-                                <a href="login.html">Login Page</a>
-                            </li>
-                        </ul>
-                        <!-- /.nav-second-level -->
-                    </li>
-                </ul>
-
-
-            </div>
-            <!-- /.sidebar-collapse -->
-        </div>
-        <!-- /.navbar-static-side -->
-    </nav>
- <div class="navbar-default sidebar" role="navigation">
+                    <div class="navbar-default sidebar" role="navigation">
         <div class="sidebar-nav navbar-collapse">
             <ul class="nav" id="side-menu">
                 <li>
@@ -320,50 +282,44 @@
         </div>
 
     </div>
+ </ul>
 
+ </div>
 </div>
+</nav>
 
+    <!-- Page Content -->
+    <div id="page-wrapper">
+        <div class="container-fluid">
 
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Page Title</h1>
 
-
-
-
-<!-- Page Content -->
-<div id="page-wrapper">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header"></h1>
-
-                @yield('content')
+                    @yield('content')
+                </div>
             </div>
-            <!-- /.col-lg-12 -->
+
+            <!-- ... Your content goes here ... -->
+
         </div>
-        <!-- /.row -->
     </div>
-    <!-- /.container-fluid -->
-</div>
-<!-- /#page-wrapper -->
 
 </div>
-<!-- /#wrapper -->
 
 <!-- jQuery -->
-<script src="js/app.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.js"></script>
-<script src="js/metisMenu.js"></script>
-<script src="js/sb-admin-2.js"></script>
-<script src="js/scripts.js"></script>
+<script src="js/jquery.min.js"></script>
 
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="js/metisMenu.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="js/startmin.js"></script>
 
 @yield('footer')
 
-
-
-
-
 </body>
-
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,7 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
- |
+ |    <link href="{{asset('css/libs.css')}}" rel="stylesheet">
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
@@ -13,3 +13,28 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+
+// mix.styles([
+
+//     'resources/assets/css/libs/blog-post.css',
+//     'resources/assets/css/libs/bootstrap.css',
+//     'resources/assets/css/libs/bootstrap-min.css',
+//     'resources/assets/css/libs/font-awesome.css',
+//     'resources/assets/css/libs/metisMenu.css',
+//     'resources/assets/css/libs/sb-admin-2.css',
+//     'resources/assets/css/libs/styles.css'
+
+
+//     ], 'public/css/libs.css');
+
+// mix.scripts([
+
+//     'resources/assets/js/libs/bootstrap.js', 
+//     'resources/assets/js/libs/bootstrap.min.js',
+//     'resources/assets/js/libs/jquery.js',
+//     'resources/assets/js/libs/metisMenu.js',
+//     'resources/assets/js/libs/sb-admin-2.js',
+//     'resources/assets/js/libs/scripts.js'
+
+//    ], 'public/js/libs.js');    
