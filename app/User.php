e1d47c9a9808c6e4cbd4f41c10481ea26f2f011d
erictumuti <erictumuti114@gmail.com>
2020-06-21 23:56:46 +0300

    relationships set up

--- app/User.php
+++ app/User.php
@@ -53,4 +53,8 @@ class User extends Authenticatable
         }
          return false;
     }
+
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
 }
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,4 +3,35 @@
 @section('content')
 
 <h1>Posts</h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">User</th>
+      <th scope="col">Category</th>
+      <th scope="col">Photo</th>
+      <th scope="col">Title</th>
+      <th scope="col">Gody</th>
+      <th scope="col">Created at</th>
+      <th scope="col">Updated at</th>
+    </tr>
+  </thead>
+  <tbody>
+      @if('$posts')
+      @foreach($posts as $post)
+    <tr>
+      <th scope="row">{{$post->id}}</th>
+      <td scope="row">{{$post->user->name}}</td>
+      <td scope="row">{{$post->category_id}}</td>
+      <td scope="row">{{$post->photo_id}}</td>
+      <td scope="row">{{$post->title}}</td>
+      <td scope="row">{{$post->body}}</td>
+      <td scope="row">{{$post->created_at->diffForHumans()}}</td>
+      <td scope="row">{{$post->updated_at->diffForHumans()}}</td>
+
+    </tr>
+    @endforeach
+    @endif
+  </tbody>
+</table>
 @endsection
